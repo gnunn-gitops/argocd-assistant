eb085368bf12d1b0817efedb3a782a451654f9f4
Gerald Nunn <gerald.b.nunn@gmail.com>
2025-04-04 11:08:32 -0700

    Switch to using a chatbot component instead of rolling own

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,66 @@
 import * as React from "react";
-import Chat from "./components/chat";
+import ChatBot, { Options } from "react-chatbotify";
+import {v4 as uuidv4} from 'uuid';
+
+import {Model, Provider, QueryRequest, QueryResponse} from "./model/service";
+import {submitQuery} from "./service/query";
+
+import "./index.css"
 
 export const Extension = (props: any) => {
 
-  const { resource, application } = props;
+    const conversationID = uuidv4();
 
-  console.log("Index Application");
-  console.log(application);
+    const { application } = props;
+    const options: Options = {
+        theme: {
+            showHeader: false,
+            showFooter: false,
+            embedded: true
 
-  return (
-    <div style = {{height:"100vh"}}>
-        <Chat resource={resource} application={application}/>
-    </div>
-  );
+        },
+        chatHistory: {
+            storageKey: "lightspeed"
+        },
+        chatWindow: {
+            showScrollbar: true
+        },
+        chatWindowStyle: {
+            width: "100%",
+            height: "100%"
+        }
+    }
+
+    const flow = {
+        start: {
+            message: "Welcome to OpenShift Lightspeed, how can I help you?",
+            path: "loop"
+        },
+		loop: {
+			message: async (params) => {
+                const queryRequest: QueryRequest = {
+                    conversation_id: conversationID,
+                    model: Model.GPT4,
+                    provider: Provider.AZURE,
+                    query: params.userInput,
+                    system_prompt: ""
+                }
+
+                const result: QueryResponse = await submitQuery(queryRequest, application);
+
+				return result.response;
+			},
+			path: "loop",
+		}
+    }
+
+    return (
+        <ChatBot options={options} flow={flow} />
+    );
 };
 
 export const component = Extension;
 
 ((window: any) => {
-    window?.extensionsAPI?.registerResourceExtension(component, '**', '*', 'Lightspeed', {icon: 'fa-sharp fa-light fa-bars-progress fa-lg'});
+    window?.extensionsAPI?.registerResourceExtension(component, '**', '*', 'Lightspeed', { icon: 'fa-sharp fa-light fa-bars-progress fa-lg' });
 })(window);
