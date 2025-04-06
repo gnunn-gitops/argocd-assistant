b293736eaba809d768a7276b5964ca01e64591b1
Gerald Nunn <gerald.b.nunn@gmail.com>
2025-04-05 19:17:14 -0700

    Add support for attaching manifest and events

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,27 @@
 import * as React from "react";
 import ChatBot, { Options } from "react-chatbotify";
 import {v4 as uuidv4} from 'uuid';
+//import * as YAML from 'yaml';
 
-import {Model, Provider, QueryRequest, QueryResponse} from "./model/service";
+import {Attachment, AttachmentTypes, Events, Model, Provider, QueryRequest, QueryResponse} from "./model/service";
 import {submitQuery} from "./service/query";
 
 import "./index.css"
 
 export const Extension = (props: any) => {
+    const [events, setEvents] = React.useState<Events>({
+        apiVersion: "v1",
+        items: []
+    });
 
     const conversationID = uuidv4();
 
-    const { application } = props;
+    const { resource, application } = props;
+
+    console.log(resource);
+
+    const application_name = application?.metadata?.name || "";
+
     const options: Options = {
         theme: {
             showHeader: false,
@@ -37,7 +47,6 @@ export const Extension = (props: any) => {
             background: "#00A2B3",
             color: "#ffffff"
         }
-
     }
 
     const flow = {
@@ -47,12 +56,41 @@ export const Extension = (props: any) => {
         },
 		loop: {
 			message: async (params) => {
+                const attachments: Attachment[] = [];
+
+                if (resource) {
+                    attachments.push(
+                        {
+                            content: JSON.stringify(resource),
+                            content_type: "application/json",
+                            attachment_type: AttachmentTypes.MANIFEST
+                        }
+                    )
+                } else {
+                    console.log("No manifest available");
+                }
+
+                if (events.items.length > 0) {
+                    attachments.push(
+                        {
+                            content: JSON.stringify(events),
+                            content_type: "application/json",
+                            attachment_type: AttachmentTypes.EVENTS
+                        }
+                    )
+                } else {
+                    console.log("No events available");
+                }
+
+                console.log("Attachments");
+                console.log(attachments);
                 const queryRequest: QueryRequest = {
                     conversation_id: conversationID,
                     model: Model.GPT4,
                     provider: Provider.AZURE,
                     query: params.userInput,
-                    system_prompt: ""
+                    system_prompt: "",
+                    attachments: attachments
                 }
 
                 const result: QueryResponse = await submitQuery(queryRequest, application);
@@ -63,6 +101,25 @@ export const Extension = (props: any) => {
 		}
     }
 
+    // Get Events
+    React.useEffect(() => {
+        let url = `/api/v1/applications/${application_name}/events?resourceUID=${resource.metadata.uid}&resourceNamespace=${resource.metadata.namespace}&resourceName=${resource.metadata.name}`;
+        if (resource.kind === "Application") {
+          url = `/api/v1/applications/${application_name}/events`;
+        }
+        fetch(url)
+          .then(response => response.json())
+          .then(data => {
+            setEvents({
+                apiVersion: "v1",
+                items: data.items
+            });
+          })
+          .catch(err => {
+            console.error("res.data", err);
+          });
+      }, [application, resource, application_name]);
+
     return (
         <ChatBot options={options} flow={flow} />
     );
@@ -71,5 +128,5 @@ export const Extension = (props: any) => {
 export const component = Extension;
 
 ((window: any) => {
-    window?.extensionsAPI?.registerResourceExtension(component, '**', '*', 'Lightspeed', { icon: 'fa-sharp fa-light fa-bars-progress fa-lg' });
+    window?.extensionsAPI?.registerResourceExtension(component, '**', '*', 'Lightspeed', { icon: 'fa-sharp fa-light fa-circle-question fa-lg' });
 })(window);
--- src/model/service.ts
+++ src/model/service.ts
@@ -5,7 +5,26 @@ export enum Provider {
 export enum Model {
     GPT4 = "gpt-4"
 }
+
+export type Events = {
+    apiVersion: string,
+    items: any[]
+}
+
+export enum AttachmentTypes {
+    EVENTS = 'event',
+    LOG = 'log',
+    MANIFEST = 'api object'
+}
+
+export type Attachment = {
+    attachment_type: AttachmentTypes,
+    content_type: string,
+    content: string
+}
+
 export type QueryRequest = {
+    attachments?: Attachment[],
     conversation_id: string,
     model: Model,
     provider: Provider,
